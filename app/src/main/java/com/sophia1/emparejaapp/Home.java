cb39afb33c4de50197d734e7e015788f340eb9d9
soffarciniegas17 <sofiarciniegas17@gmail.com>
2018-07-25 11:13:27 -0500

    tpskmlfbkjbk

--- app/src/main/java/com/sophia1/emparejaapp/Home.java
+++ app/src/main/java/com/sophia1/emparejaapp/Home.java
@@ -1,8 +1,10 @@
 package com.sophia1.emparejaapp;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.provider.Telephony;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.TextureView;
@@ -13,9 +15,9 @@ import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
 
-    TextView t;
+    TextView t, p1, p2, p3, p4;
     EditText j;
-    Button si;
+    Button si, easy, hard, medium;
     Dialog registry, settings, scores;
     String nick1, nick2;
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,26 +30,32 @@ public class Home extends AppCompatActivity {
 
         settings = new Dialog(this);
         settings.setContentView(R.layout.dialog_settings);
+
+        settings = new Dialog(this);
+        settings.setContentView(R.layout.dialog_settings);
         ingresarNombres();
     }
 
 int comprueba=0;
     public void ingresarNombres (){
         si = registry.findViewById(R.id.okey);
-        t= registry.findViewById(R.id.textV);
+        t= registry.findViewById(R.id.texto);
         j= registry.findViewById(R.id.editText);
         si.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (comprueba==0){
+                    t= registry.findViewById(R.id.texto);
                     t.setText("1 JUGADOR");
                     nick1=  j.getText().toString();
+                    t.setText("");
                     if (nick1.equalsIgnoreCase("")){
                         nick1= "USSER 1";
                     }
                     comprueba=1;
                     ingresarNombres();
                 } else {
+                    t= registry.findViewById(R.id.texto);
                     t.setText("2 JUGADOR");
                     nick2 = j.getText().toString();
                     if (nick2.equalsIgnoreCase("")){
@@ -61,13 +69,24 @@ int comprueba=0;
 
 
     public void menu (View v){
+        Intent i;
         switch (v.getId()){
             case R.id.juego:
+                i = new Intent(this, Partida.class);
+                startActivity(i);
                 break;
-                case R.id.
+            case R.id.scoress:
+                break;
         }
     }
 
+    public void puntajes (){
+        easy = settings.findViewById(R.id.easy);
+        medium = settings.findViewById(R.id.medium);
+        hard = settings.findViewById(R.id.hard);
+
+     //   p1 =
+    }
     @Override
     protected void onResume() {
         super.onResume();
