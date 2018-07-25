655a6e02ba08892d7b55a5320845f6995a102732
soffarciniegas17 <sofiarciniegas17@gmail.com>
2018-07-25 10:43:06 -0500

    tpskmlf

---
+++ app/src/main/java/com/sophia1/emparejaapp/Home.java
@@ -0,0 +1,95 @@
+package com.sophia1.emparejaapp;
+
+import android.app.Dialog;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Home extends AppCompatActivity {
+
+    TextView t;
+    EditText j;
+    Button si;
+    Dialog registry, settings, scores;
+    String nick1, nick2;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        registry = new Dialog(this);
+        registry.setContentView(R.layout.dialog_names);
+
+
+        settings = new Dialog(this);
+        settings.setContentView(R.layout.dialog_settings);
+        ingresarNombres();
+    }
+
+int comprueba=0;
+    public void ingresarNombres (){
+        si = registry.findViewById(R.id.okey);
+        t= registry.findViewById(R.id.textV);
+        j= registry.findViewById(R.id.editText);
+        si.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (comprueba==0){
+                    t.setText("1 JUGADOR");
+                    nick1=  j.getText().toString();
+                    if (nick1.equalsIgnoreCase("")){
+                        nick1= "USSER 1";
+                    }
+                    comprueba=1;
+                    ingresarNombres();
+                } else {
+                    t.setText("2 JUGADOR");
+                    nick2 = j.getText().toString();
+                    if (nick2.equalsIgnoreCase("")){
+                        nick2="USSER 2";
+                    }
+                    registry.dismiss();}
+            }
+        });
+    registry.show();
+    }
+
+
+    public void menu (View v){
+        switch (v.getId()){
+            case R.id.juego:
+                break;
+                case R.id.
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences recupera = PreferenceManager.getDefaultSharedPreferences(this);
+
+        nick1= recupera.getString("nick1", "Jugador 1");
+        nick2= recupera.getString("nick2", "Jugador 2");
+
+    }
+
+    @Override
+    protected void onPause() {
+
+        SharedPreferences datos = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = datos.edit();
+
+        editor.putString("nick1", nick1);
+        editor.putString("nick2", nick2);
+
+        editor.apply();
+        super.onPause();
+    }
+
+
+}
--- app/src/main/java/com/sophia1/emparejaapp/Splash_screen.java
+++ app/src/main/java/com/sophia1/emparejaapp/Splash_screen.java
@@ -1,5 +1,7 @@
 package com.sophia1.emparejaapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,18 @@ public class Splash_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        tiempo();
+    }
+
+
+    public void tiempo(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash_screen.this, Home.class);
+                startActivity(i);
+                finish();
+            }
+        },4000);
     }
 }
