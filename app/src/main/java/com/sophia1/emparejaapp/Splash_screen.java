f9b3613410cb6326e976cc81cb5f92acc740628b
soffarciniegas17 <sofiarciniegas17@gmail.com>
2018-07-25 09:28:28 -0500

    Initial commit

---
+++ app/src/main/java/com/sophia1/emparejaapp/Splash_screen.java
@@ -0,0 +1,13 @@
+package com.sophia1.emparejaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
